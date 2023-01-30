ce0d7771c9b207cecf718b637add57ea54c0b8ee
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2023-01-30 13:24:24 +0100

    * Fixed loop issue when two phone battery complications are set on the same watch face * min time between two complication updates = 5sec * Gradle update 7.5.0

--- wear/src/main/java/com/weartools/phonebattcomp/MobileBatteryComplicationTapBroadcastReceiver.kt
+++ wear/src/main/java/com/weartools/phonebattcomp/MobileBatteryComplicationTapBroadcastReceiver.kt
@@ -38,42 +38,50 @@ class MobileBatteryComplicationTapBroadcastReceiver : BroadcastReceiver() {
 
         val result = goAsync()
         val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-        val hasMobileApp = preferences.getBoolean(context.getString(R.string.key_pref_has_mobile_app), false)
-        val lastUpdateTime = preferences.getLong(context.getString(R.string.key_pref_last_update), 0)
+        val hasMobileApp =
+            preferences.getBoolean(context.getString(R.string.key_pref_has_mobile_app), false)
+        val lastUpdateTime =
+            preferences.getLong(context.getString(R.string.key_pref_last_update), 0)
 
         scope.launch {
             try {
-                        if (!hasMobileApp) {
-                            openAppStoreOnPhone(context = context)
-                            Log.d(TAG, "OPENING APP ON PHONE IF NEEDED")
-                            Toast.makeText(
-                                context,
-                                context.getString(R.string.install_companion),
-                                Toast.LENGTH_LONG
-                            ).show()
-                            }
-                        else
-                            Log.d(TAG, "UPDATING BATTERY COMPLICATION")
-                            SendMessageService.sndMSG(context,"/request_battery", lastUpdateTime)
+                if (!hasMobileApp) {
+                    openAppStoreOnPhone(context = context)
+                    Log.d(TAG, "OPENING APP ON PHONE IF NEEDED")
+                    Toast.makeText(
+                        context,
+                        context.getString(R.string.install_companion),
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else
+                    Log.d(TAG, "UPDATING BATTERY COMPLICATION")
+                SendMessageService.sndMSG(context, "/request_battery", lastUpdateTime)
+            } finally {
+                result.finish()
             }
-            finally { result.finish() }
         }
     }
 
     fun openAppStoreOnPhone(context: Context) {
         val remoteActivityHelper = RemoteActivityHelper(context)
-            val intentAndroid = Intent(Intent.ACTION_VIEW)
-                .addCategory(Intent.CATEGORY_BROWSABLE)
-                .setData(Uri.parse(MainActivity.PLAY_STORE_APP_URI))
-            remoteActivityHelper.startRemoteActivity(intentAndroid,targetNodeId = null)
+        val intentAndroid = Intent(Intent.ACTION_VIEW)
+            .addCategory(Intent.CATEGORY_BROWSABLE)
+            .setData(Uri.parse(MainActivity.PLAY_STORE_APP_URI))
+        remoteActivityHelper.startRemoteActivity(intentAndroid, targetNodeId = null)
     }
 
     companion object {
         private val TAG = MobileBatteryComplicationTapBroadcastReceiver::class.java.simpleName
-        private const val EXTRA_DATA_SOURCE_COMPONENT = "com.example.android.wearable.complicationsdatasource.action.DATA_SOURCE_COMPONENT"
-        private const val EXTRA_COMPLICATION_ID = "com.example.android.wearable.complicationsdatasource.action.COMPLICATION_ID"
+        private const val EXTRA_DATA_SOURCE_COMPONENT =
+            "com.example.android.wearable.complicationsdatasource.action.DATA_SOURCE_COMPONENT"
+        private const val EXTRA_COMPLICATION_ID =
+            "com.example.android.wearable.complicationsdatasource.action.COMPLICATION_ID"
 
-        fun getToggleIntent(context: Context, dataSource: ComponentName, complicationId: Int): PendingIntent {
+        fun getToggleIntent(
+            context: Context,
+            dataSource: ComponentName,
+            complicationId: Int
+        ): PendingIntent {
             val intent = Intent(context, MobileBatteryComplicationTapBroadcastReceiver::class.java)
             intent.putExtra(EXTRA_DATA_SOURCE_COMPONENT, dataSource)
             intent.putExtra(EXTRA_COMPLICATION_ID, complicationId)
