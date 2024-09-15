8636c3e20f33842c1608f67a9f18966cc6d13d36
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2024-09-15 07:46:35 +0200

    * WatchBatteryComplicationService improvements 8

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.weartools.phonebattcomp"
         minSdk = 27
         targetSdk = 34
-        versionCode = 10000419
-        versionName = "4.1.9"
+        versionCode = 10000420
+        versionName = "4.2.0"
 
         buildConfigField("String", "CAPABILITY_MOBILE_APP", "\"phonebattcomp_mobile_app\"")
         buildConfigField("String", "PLAY_STORE_APP_URI", "\"market://details?id=com.weartools.phonebattcomp\"")
