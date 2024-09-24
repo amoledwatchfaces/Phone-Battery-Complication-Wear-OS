0da2764f8194cffe90068d47bc9d107f6f9f8cd9
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2024-09-24 14:16:54 +0200

    * calendar events final PR 5

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.weartools.phonebattcomp"
         minSdk = 27
         targetSdk = 34
-        versionCode = 10000456
-        versionName = "4.5.6"
+        versionCode = 10000457
+        versionName = "4.5.7"
 
         buildConfigField("String", "CAPABILITY_MOBILE_APP", "\"phonebattcomp_mobile_app\"")
         buildConfigField("String", "PLAY_STORE_APP_URI", "\"market://details?id=com.weartools.phonebattcomp\"")
