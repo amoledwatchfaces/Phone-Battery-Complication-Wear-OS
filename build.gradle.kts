0a87524ae2d7c43ae81ed37bbefa8732bfb71395
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2024-10-15 19:26:16 +0000

    * NotificationListener.kt update 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@ tasks.register("clean", Delete::class) {
 buildscript {
 
     /** Set version for wear & mobile modules **/
-    val versionCode by extra(10000504)
-    val versionName by extra("5.0.2")
+    val versionCode by extra(10000505)
+    val versionName by extra("5.0.5")
 
     dependencies {
         classpath ("com.android.tools.build:gradle:8.6.1")
