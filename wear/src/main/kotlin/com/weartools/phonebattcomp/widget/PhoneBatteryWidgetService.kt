50c7c9d2f83bf3f21947f9168cf62d22e12bc6ae
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2026-08-24 10:22:45 +0000

    * widget setup 5

--- wear/src/main/kotlin/com/weartools/phonebattcomp/widget/PhoneBatteryWidgetService.kt
+++ wear/src/main/kotlin/com/weartools/phonebattcomp/widget/PhoneBatteryWidgetService.kt
@@ -1,15 +1,22 @@
 package com.weartools.phonebattcomp.widget
 
 import android.content.Context
+import androidx.compose.remote.creation.compose.capture.RemoteImageVector
 import androidx.compose.remote.creation.compose.layout.RemoteAlignment
+import androidx.compose.remote.creation.compose.layout.RemoteArrangement
 import androidx.compose.remote.creation.compose.layout.RemoteBox
+import androidx.compose.remote.creation.compose.layout.RemoteColumn
 import androidx.compose.remote.creation.compose.layout.RemoteComposable
+import androidx.compose.remote.creation.compose.layout.RemoteRow
 import androidx.compose.remote.creation.compose.modifier.RemoteModifier
-import androidx.compose.remote.creation.compose.modifier.fillMaxSize
-import androidx.compose.remote.creation.compose.state.rc
+import androidx.compose.remote.creation.compose.modifier.fillMaxWidth
+import androidx.compose.remote.creation.compose.state.animateRemoteFloat
+import androidx.compose.remote.creation.compose.state.rb
+import androidx.compose.remote.creation.compose.state.rdp
+import androidx.compose.remote.creation.compose.state.rememberMutableRemoteFloat
+import androidx.compose.remote.creation.compose.state.rf
 import androidx.compose.remote.creation.compose.state.rs
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.datastore.core.DataStore
 import androidx.glance.wear.AssociateWithGlanceWearWidget
 import androidx.glance.wear.GlanceWearWidget
@@ -19,6 +26,10 @@ import androidx.glance.wear.WearWidgetData
 import androidx.glance.wear.WearWidgetDocument
 import androidx.glance.wear.color
 import androidx.glance.wear.core.WearWidgetParams
+import androidx.wear.compose.remote.material3.RemoteCircularProgressIndicator
+import androidx.wear.compose.remote.material3.RemoteColorScheme
+import androidx.wear.compose.remote.material3.RemoteIcon
+import androidx.wear.compose.remote.material3.RemoteProgressIndicatorDefaults
 import androidx.wear.compose.remote.material3.RemoteText
 import com.weartools.phonebattcomp.data.UserPreferences
 import dagger.hilt.android.AndroidEntryPoint
@@ -50,8 +61,44 @@ class PhoneBatteryWidget(
         // Read the snapshot of user preferences
         val preferences: UserPreferences = dataStore.data.first()
 
-        return WearWidgetDocument(background = WearWidgetBrush.color(WIDGET_COLOR_SCHEME.surfaceContainerHigh)) {
-            PhoneBatteryWidgetContent(preferences)
+        val appColorScheme = WIDGET_COLOR_SCHEME
+
+        return WearWidgetDocument(background = WearWidgetBrush.color(appColorScheme.surfaceContainerHigh)) {
+
+            PhoneBatteryWidgetContent(
+                appColorScheme =  appColorScheme,
+                batteryLevel = 75,
+                nodeName = "Pixel 9 Pro XL",
+                chargeRemainingTime = formatChargeTimeRemaining(
+                    300000,
+                    true,
+                    true,
+                    75
+                ),
+                phoneIsConnected = true,
+                phoneIsCharging = true,
+                materialSymbols = true,
+                chargingSymbolInside = false
+            )
+            // Original
+            /*
+            PhoneBatteryWidgetContent(
+                appColorScheme =  appColorScheme,
+                batteryLevel = preferences.phoneBatteryLevel,
+                nodeName = preferences.nodeName,
+                chargeRemainingTime = formatChargeTimeRemaining(
+                    preferences.chargeRemainingTime,
+                    preferences.phoneIsConnected,
+                    preferences.phoneIsCharging,
+                    preferences.phoneBatteryLevel
+                ),
+                phoneIsConnected = preferences.phoneIsConnected,
+                phoneIsCharging = preferences.phoneIsCharging,
+                materialSymbols = preferences.materialSymbols,
+                chargingSymbolInside = preferences.chargingSymbolInsideIcon
+            )
+
+             */
         }
     }
 
@@ -78,14 +125,60 @@ class PhoneBatteryWidget(
 
 @RemoteComposable
 @Composable
-fun PhoneBatteryWidgetContent(preferences: UserPreferences) {
-    RemoteBox(
-        modifier = RemoteModifier.fillMaxSize(),
-        contentAlignment = RemoteAlignment.Center,
+fun PhoneBatteryWidgetContent(
+    appColorScheme: RemoteColorScheme,
+    batteryLevel: Int,
+    nodeName: String,
+    chargeRemainingTime: String,
+    phoneIsConnected: Boolean,
+    phoneIsCharging: Boolean,
+    materialSymbols: Boolean,
+    chargingSymbolInside: Boolean
+) {
+    RemoteColumn(
+        modifier = RemoteModifier.fillMaxWidth(),
+        verticalArrangement = RemoteArrangement.Top,
+        horizontalAlignment = RemoteAlignment.Start,
     ) {
-        RemoteText(
-            text = "Hello World".rs,
-            color = Color.White.rc
-        )
+        RemoteRow() {
+            RemoteText(
+                text = nodeName.rs,
+                color = appColorScheme.onSurfaceVariant
+            )
+        }
+        RemoteRow() {
+            RemoteColumn() {
+                RemoteText(
+                    text = "${batteryLevel}%".rs,
+                    color = appColorScheme.onSecondaryContainer
+                )
+                RemoteText(
+                    text = chargeRemainingTime.rs,
+                    color = appColorScheme.secondary
+                )
+            }
+            RemoteColumn() {
+                RemoteBox(
+                    contentAlignment = RemoteAlignment.Center
+                ){
+                    val progress = rememberMutableRemoteFloat { (1F * batteryLevel / 100).rf }
+                    val animatedProgress = animateRemoteFloat(progress, 0.25f)
+
+                    RemoteCircularProgressIndicator(
+                        gapSize = 4.rdp,
+                        progress = animatedProgress,
+                        startAngle = 110.rf,
+                        endAngle = 430.rf,
+                        enabled = phoneIsConnected.rb
+                    )
+
+                    RemoteIcon(
+                        imageVector = RemoteImageVector.fromResource()
+                    )
+
+                }
+            }
+
+        }
     }
 }
