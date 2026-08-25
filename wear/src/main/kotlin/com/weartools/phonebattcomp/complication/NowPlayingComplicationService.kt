e132e3763efd06230f6b15fc725c1369e7ab4a19
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2026-08-25 09:43:25 +0000

    * media playback 11

--- wear/src/main/kotlin/com/weartools/phonebattcomp/complication/NowPlayingComplicationService.kt
+++ wear/src/main/kotlin/com/weartools/phonebattcomp/complication/NowPlayingComplicationService.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2022 amoledwatchfaces™
- * support@amoledwatchfaces.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.weartools.phonebattcomp.complication
 
 import android.graphics.BitmapFactory
@@ -89,12 +73,12 @@ class NowPlayingComplicationService : SuspendingComplicationDataSourceService() 
                 LongTextComplicationData.Builder(
                     text = PlainComplicationText.Builder(text = if (hasMedia) title else getString(R.string.no_media)).build(),
                     contentDescription = ComplicationText.EMPTY)
-                    .apply {
-                        if (hasMedia) {
-                            setTitle(PlainComplicationText.Builder(text = artist).build())
-                            setSmallImage(SmallImage.Builder(image = icon, type = SmallImageType.PHOTO).build())
-                        }
-                    }
+                    .setTitle(if (artist.isNotEmpty()) PlainComplicationText.Builder(text = artist).build() else null)
+                    .setSmallImage(SmallImage.Builder(
+                        image = icon,
+                        type = SmallImageType.PHOTO)
+                        .setAmbientImage(icon)
+                        .build())
                     .setTapAction(tapIntent)
                     .build()
             }
