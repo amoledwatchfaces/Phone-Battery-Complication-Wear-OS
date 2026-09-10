673cc64eb590bfe6bff2585b74147894ebbc893e
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2026-09-10 08:30:57 +0000

    * remove openAppStoreOnPhone function

--- wear/src/main/kotlin/com/weartools/phonebattcomp/utils/ContextUtils.kt
+++ wear/src/main/kotlin/com/weartools/phonebattcomp/utils/ContextUtils.kt
@@ -4,13 +4,9 @@ import android.content.ActivityNotFoundException
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
 import androidx.core.net.toUri
-import androidx.wear.remote.interactions.RemoteActivityHelper
 import androidx.wear.watchface.complications.datasource.ComplicationDataSourceUpdateRequester
 import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
-import com.weartools.phonebattcomp.BuildConfig
-import com.weartools.phonebattcomp.R
 import com.weartools.phonebattcomp.complication.EventTimerComplicationService
 import com.weartools.phonebattcomp.complication.MobileBatteryComplicationService
 import com.weartools.phonebattcomp.complication.NotificationsIcons4ComplicationService
@@ -45,15 +41,6 @@ fun Context.updateNotificationComplications() {
     updateComplication(NotificationsIconsComplicationService::class.java)
     updateComplication(NotificationsIcons4ComplicationService::class.java)
     updateComplication(NotificationsPreviewComplicationService::class.java)
-}
-
-fun Context.openAppStoreOnPhone() {
-    val remoteActivityHelper = RemoteActivityHelper(this)
-    val intentAndroid = Intent(Intent.ACTION_VIEW)
-        .addCategory(Intent.CATEGORY_BROWSABLE)
-        .setData(BuildConfig.PLAY_STORE_APP_URI.toUri())
-    remoteActivityHelper.startRemoteActivity(intentAndroid,targetNodeId = null)
-    Toast.makeText(this, this.getString(R.string.check_phone), Toast.LENGTH_LONG).show()
 }
 
 
